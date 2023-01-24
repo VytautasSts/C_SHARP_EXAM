310acc6f65889437ebf324a3ea89e8371f90b82a
VytautasSts <111770550+VytautasSts@users.noreply.github.com>
2023-01-24 22:22:43 +0200

    Uploading models, utilities and obj folders

---
+++ CSHARP_ExamTests/Models/CheckTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSHARP_Exam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP_Exam.Models.Tests
+{
+    [TestClass()]
+    public class CheckTests
+    {
+        [TestMethod()]
+        public void add_amount_to_check_Test()
+        {
+            // Arrange
+            Table table = new Table(1,1,1,(bool)true);
+            Guid sample = Guid.NewGuid();
+            Check check = new Check(table,sample);
+            double addition = 9.99;
+            double expected = 9.99;
+
+            // Act
+            check.add_amount(addition);
+
+            // Assert
+            double actual = check.Amount;
+            Assert.AreEqual(expected, actual, 0.001, "Amount did not transfer correctly");
+        }
+
+        [TestMethod()]
+        public void get_time_from_check_Test()
+        {
+            // Arrange
+            Table table = new Table(1, 1, 1, (bool)true);
+            Guid sample = Guid.NewGuid();
+            Check check = new Check(table, sample);
+
+            // Act
+            var currentdate = DateTime.Now;
+            check.Timestamp = currentdate;
+
+            // Assert
+            Assert.AreEqual(currentdate, check.Timestamp, "Date did not transfer correctly");
+        }
+    }
+}
---
+++ CSHARP_ExamTests/Utilities/AddClientsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSHARP_Exam.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSHARP_Exam.Models;
+
+namespace CSHARP_Exam.Utilities.Tests
+{
+    [TestClass()]
+    public class AddClientsTests
+    {
+        [TestMethod()]
+        public void Generate_client_list_Test()
+        {
+            // Arrange
+            AddClients addClientsGenerator = new AddClients(true);
+            List<int> clients = new List<int>();
+            addClientsGenerator.Generate(clients);
+            int sizeBefore = 0;
+            int sizeAfter = 0;
+            
+            // Act
+            while (clients.Count()==0)
+            {
+                Console.WriteLine(clients.Count());
+            };
+            sizeAfter= clients.Count();
+
+            // Assert
+            Assert.AreNotEqual(sizeBefore, sizeAfter, "Function did not generate any new members.");
+        }
+    }
+}
